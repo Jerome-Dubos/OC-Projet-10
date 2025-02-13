c1e4623f50c515a203807538a75f2951816b6854
Jérôme DUBOS <jeromedubos67@gmail.com>
2025-02-13 09:06:49 +0100

    Retrait commentaires useless

--- OC-Projet-10-Frontend/Argent-Bank/src/store/store.js
+++ OC-Projet-10-Frontend/Argent-Bank/src/store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice'; // On va créer ce slice dans l'étape suivante
+import userReducer from './userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer, // Ajouter le reducer pour gérer l'état de l'utilisateur
+    user: userReducer,
   },
 });
 
-// Exporter le hook useDispatch et useSelector pour une utilisation dans les composants
+
 export default store;
